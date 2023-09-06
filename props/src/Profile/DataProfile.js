653989376acaa54574f56505b71360b71c7ca003
MedBel2 <moebelhassen123@gmail.com>
2023-09-06 19:12:45 +0100

    first commit

---
+++ props/src/Profile/DataProfile.js
@@ -0,0 +1,22 @@
+//Notre fake Data 
+const DataProfile=[{
+    name:'MedBel',
+    imgsrc:'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/85e01db3-e56f-4b08-b5ef-9320df1b46f5/deem3ij-bf4f9b67-b7fe-4e3b-b11f-f77947b40218.png/v1/fill/w_856,h_360,q_80,strp/darwin_s_real_parents_are_incidentals_42_and_49_by_funguy2001_deem3ij-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MzYwIiwicGF0aCI6IlwvZlwvODVlMDFkYjMtZTU2Zi00YjA4LWI1ZWYtOTMyMGRmMWI0NmY1XC9kZWVtM2lqLWJmNGY5YjY3LWI3ZmUtNGUzYi1iMTFmLWY3Nzk0N2I0MDIxOC5wbmciLCJ3aWR0aCI6Ijw9ODU2In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.2X6WXPkVTexdolfyWhlL7DvdbiG6W1opVXBmhac1mQg',
+    bio:'Ons Jabeur, née le 28 août 1994 à Ksar Hellal, est une joueuse de tennis tunisienne, professionnelle depuis 2010. En devenant numéro 2 mondiale dans le classement WTA en 2022, Jabeur est devenue la joueuse tunisienne, arabe et africaine la mieux classée des classements WTA et ATP.',
+    profession:'Joueuse de tennis tunisienne ',
+},
+{
+    name:'Hama el lembi',
+    imgsrc:'https://static.wikia.nocookie.net/theamazingworldofgumball/images/5/5a/Season_3_Richard.png/revision/latest?cb=20161108234438',
+    bio:'LeBron Raymone James, né le 30 décembre 1984 à Akron, est un joueur professionnel américain de basket-ball. Il évolue actuellement aux Lakers de Los Angeles en National Basketball Association. Mesurant 2,06 m et pesant 113 kg, il évolue principalement aux postes de meneur, ailier et ailier fort',
+    profession:'Joueur de basket-ball américain',
+
+},
+{
+    name:'Kobe Bryant',
+    imgsrc:'https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Kobe_Bryant_2014.jpg/640px-Kobe_Bryant_2014.jpg',
+    bio:'Kobe Bryant, né le 23 août 1978 à Philadelphie en Pennsylvanie et mort le 26 janvier 2020 à Calabasas en Californie, est un joueur américain de basket-ball. Il évolue dans la franchise NBA des Lakers de Los Angeles pendant vingt saisons, entre 1996 et 2016',
+    profession:'Joueur de basket-ball américain',
+
+}]
+export default DataProfile;
